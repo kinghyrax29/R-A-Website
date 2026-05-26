62157dc91c749789b181e00a00d4317b33746cb4
kinghyrax29 <acevedoaaron29@gmail.com>
2026-05-26 17:39:41 -0400

    chatgpt site

--- Website/js/scripts.js
+++ Website/js/scripts.js
@@ -1,10 +1,11 @@
-/*!
-* Start Bootstrap - Business Casual v7.0.9 (https://startbootstrap.com/theme/business-casual)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-business-casual/blob/master/LICENSE)
-*/
-// Highlights current date on contact page
-window.addEventListener('DOMContentLoaded', event => {
-    const listHoursArray = document.body.querySelectorAll('.list-hours li');
-    listHoursArray[new Date().getDay()].classList.add(('today'));
-})
+window.addEventListener("scroll", () => {
+
+  const header = document.querySelector("header");
+
+  if (window.scrollY > 50) {
+    header.style.boxShadow = "0 4px 20px rgba(0,0,0,0.08)";
+  } else {
+    header.style.boxShadow = "none";
+  }
+
+});
